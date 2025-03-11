81c96e3d7a5dbb4a7950ccb207c9b315711f0cc3
Ivan Zahariev <famzah@famzah.net>
2025-03-11 21:56:16 +0200

    Dedup code

--- server_example.py
+++ server_example.py
@@ -6,8 +6,10 @@ from websocket_handler import WebSocketHandler
 import logging
 import json
 import signal
+import argparse
 
 shutdown_event = asyncio.Event()
+cmd_args = None
 
 async def auth_callback(message, websocket, logger):
     logger.debug(f'Got auth message: {message}')
@@ -22,19 +24,39 @@ async def auth_callback(message, websocket, logger):
     return False
 
 async def handle_connection(websocket):
+    if cmd_args.auth_mode != 'noauth':
+        auth_func = auth_callback
+    else:
+        auth_func = None
+
     handler = WebSocketHandler(
-        websocket, auth_callback,
+        websocket, auth_func,
         shutdown_event = shutdown_event,
         #log_level=logging.DEBUG
     )
 
     await handler.run()
+    #print("handle_connection(): WebSocket is completely closed")
 
 def handle_ctrl_c():
     shutdown_event.set()
     print("\nWebSocket server got CTRL+C")
 
+def parse_argv():
+    parser = argparse.ArgumentParser(description="Example WebSocket server.")
+
+    parser.add_argument(
+        "auth_mode",
+        choices=["auth", "noauth"],
+        help="authentication mode (choose 'auth' for authentication or 'noauth' for no authentication)"
+    )
+
+    return parser.parse_args()
+
 async def main():
+    global cmd_args
+    cmd_args = parse_argv()
+
     loop = asyncio.get_running_loop()
     loop.add_signal_handler(signal.SIGINT, handle_ctrl_c)
 
